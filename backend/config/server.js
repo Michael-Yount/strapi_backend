65d35471c17646461b9435a177af9b8bc721dc6e
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-09-16 01:23:41 -0400

    url

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url:'https://restaurant-strapi.onrender.com")',
   app: {
     keys: env.array('APP_KEYS'),
-    url:env("https://restaurant-strapi.onrender.com"),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
