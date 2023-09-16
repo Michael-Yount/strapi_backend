f8f016ae3fa44eaa9b2b3067574579c5a88495f5
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-09-16 01:31:48 -0400

    env

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url:'https://restaurant-strapi.onrender.com")',
+  url:env('https://restaurant-strapi.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
